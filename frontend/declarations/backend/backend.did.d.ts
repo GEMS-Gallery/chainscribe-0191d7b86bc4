972fd26bb4499aa34640b553e02e2e1a850070f4
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-09 13:14:02 -0400

    0191d7c6-f19a-777b-861d-f636c00f6c08

--- frontend/declarations/backend/backend.did.d.ts
+++ frontend/declarations/backend/backend.did.d.ts
@@ -2,17 +2,4 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface Game {
-  'date' : string,
-  'time' : string,
-  'week' : bigint,
-  'isHome' : boolean,
-  'opponent' : Team,
-}
-export type Team = string;
-export interface _SERVICE {
-  'generateSchedule' : ActorMethod<[Team], Array<Game>>,
-  'getTeams' : ActorMethod<[], Array<Team>>,
-}
-export declare const idlFactory: IDL.InterfaceFactory;
-export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
+
--- frontend/declarations/backend/backend.did.js
+++ frontend/declarations/backend/backend.did.js
@@ -1,15 +1,1 @@
-export const idlFactory = ({ IDL }) => {
-  const Team = IDL.Text;
-  const Game = IDL.Record({
-    'date' : IDL.Text,
-    'time' : IDL.Text,
-    'week' : IDL.Nat,
-    'isHome' : IDL.Bool,
-    'opponent' : Team,
-  });
-  return IDL.Service({
-    'generateSchedule' : IDL.Func([Team], [IDL.Vec(Game)], []),
-    'getTeams' : IDL.Func([], [IDL.Vec(Team)], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
+
